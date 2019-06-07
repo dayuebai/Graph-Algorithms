dd4cf752f88d470f8fe8188b12de4f41502ac04e
dayuebai <dayueb@uci.edu>
2019-06-07 15:21:13 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,12 +5,6 @@ g++ -std=c++14 main.cpp -o proj3
 echo Finish compiling successfully.
 
 echo Start running...
-if [[ "$OSTYPE" == "linux-gnu" ]]; then
-    echo System OS: Linux-GNU
-    valgrind ./proj3
-else
-    echo Other system OS
-    ./proj3
-fi
+./proj3
 rm ./proj3
 echo Finish running successfully.
